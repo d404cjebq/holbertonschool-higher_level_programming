104c1af9030dd07f326a17da284d2872e3eb07be
d404cjebq <imdhaydhw@gmail.com>
2026-06-03 15:17:26 +0300

    add Positive anything is better than negative nothing

--- 0-positive_or_negative.py
+++ 0-positive_or_negative.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+# Positive anything is better than negative nothing
 import random
 number = random.randint(-10, 10)
-
-# Positive anything is better than negative nothing
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
