0dcbfe316a7ae6af3eb1f1f7f7eb3f414d601de6
d404cjebq <imdhaydhw@gmail.com>
2026-06-10 16:29:50 +0300

    Task 0 add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,22 +3,18 @@
 
 
 def add_integer(a, b=98):
-    """Add two integers.
+    """Adds two integers."""
 
-    Args:
-        a: First number.
-        b: Second number (default 98).
-
-    Returns:
-        The integer addition of a and b.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
-    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    try:
+        a = int(a)
+        b = int(b)
+    except OverflowError:
+        raise OverflowError
+
+    return a + b
