f38d3a45142d18a923f21eea56caa97c85dde34e
d404cjebq <imdhaydhw@gmail.com>
2026-06-22 16:31:35 +0300

    add task9

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Module that defines BaseGeometry class."""
+"""Module that defines BaseGeometry."""
 
 
 class BaseGeometry:
     """BaseGeometry class."""
 
     def area(self):
-        """Raises an exception because area is not implemented."""
+        """Raises an exception."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
