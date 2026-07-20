5651b9e3ab1c3dfc2ded30c10d64b91efe3c3435
d404cjebq <imdhaydhw@gmail.com>
2026-07-20 20:57:52 +0300

    add9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,12 @@
---  script that creates a table
+-- Create the table if it does not exist
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table (id, name, score)
-VALUES (1,"John",10);
-VALUES (2,"Alex",3);
-VALUES (3,"Bob",14);
-VALUES (4,"George",8);
+-- Insert multiple rows into the table
+INSERT INTO second_table (id, name, score) VALUES 
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
