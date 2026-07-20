8144d71d551547fc451c32767fe3c11ad96b826a
d404cjebq <imdhaydhw@gmail.com>
2026-07-20 21:00:43 +0300

    add9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS second_table (
     score INT
 );
 -- second_table description:
-INSERT INTO second_table (id, name, score)
-VALUES (1,"John",10);
-VALUES (2,"Alex",3);
-VALUES (3,"Bob",14);
-VALUES (4,"George",8);
+INSERT INTO second_table (id, name, score) VALUES 
+(1,"John",10),
+(2,"Alex",3),
+(3,"Bob",14),
+(4,"George",8);
