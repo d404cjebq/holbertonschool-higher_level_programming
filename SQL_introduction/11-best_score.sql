bf37fab95a45aec320dbccc3a6f051531e208150
d404cjebq <imdhaydhw@gmail.com>
2026-07-20 21:21:17 +0300

    add11

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- script that lists all records with a score >= 10
-SELECT score , name FROM second_table
+SELECT score, name FROM second_table
 WHERE score >= 10;
-ORDER BY score;
+ORDER BY score DESC;
