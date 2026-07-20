52e3abf7080ddae15d5b37f012324b6cd3f34696
d404cjebq <imdhaydhw@gmail.com>
2026-07-20 20:14:02 +0300

    add task7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
 -- script that inserts a new row in the table
-INSERTS INTO first_table (New row:,id = 89,name = Best School);
+INSERTS INTO first_table 
+VALUES (New row:,id = 89,name = Best School);
