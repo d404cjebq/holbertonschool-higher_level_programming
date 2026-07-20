27aa3117ef00bebb58a60958b732b21e73e19c82
d404cjebq <imdhaydhw@gmail.com>
2026-07-20 20:56:55 +0300

    add9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 --  script that creates a table
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
-    name VARCHAR(256)
+    name VARCHAR(256),
     score INT
 );
 INSERT INTO second_table (id, name, score)
