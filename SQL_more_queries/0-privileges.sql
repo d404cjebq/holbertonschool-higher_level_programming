827c1069cdcd414b38232fa6c3b96204d24d0c16
d404cjebq <imdhaydhw@gmail.com>
2026-07-22 10:17:54 +0300

    add task0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- script that lists all privileges
-CREATE USER 'user_0d_1'@'localhost';
-CREATE USER 'user_0d_2'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost, user_0d_2@localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
